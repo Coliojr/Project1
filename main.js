b8f27e5bc7b95c1d49031762d7ea16014c07c3fb
Kelley-rosa <kelley.rosa@bellevuecollege.edu>
2023-02-21 19:21:48 -0800

    fixed undefined issue

--- main.js
+++ main.js
@@ -1,34 +1,11 @@
-//Main array
+/*** Main Song Array ************************************************************/
 let songArr = [];
 
-//Function to create an instance of song object
-/* let newSong = function() {
-    
-    let Title = document.getElementById("title").value;
-    let Artist = document.getElementById("artist").value;
-    let Album = document.getElementById("album").value;
-    let Year = document.getElementById("year").value;
-    let Genre = document.getElementById("genre").value;
-    let Rating = document.getElementById("rating").value;         
-
-    let song = new Song(Title, Artist, Album, Year, Genre, Rating);
-
-    if (!song.isValid()) {
-        alert("Please enter information in all input fields. " 
-        + "Year must be greater than 1 and in yyyy format. " + "Raiting must be 1-5");
-    } else {
-        songArr = [];
-        songArr.push(song);
-        console.log(songArr);
-    }
-}; */
-
-
+/*** Load After DOM *************************************************************/
 document.addEventListener("DOMContentLoaded", function () {
-
     createList();
 
-    //Buttons -----------------------------------------------------------------
+    /* Button: Add Song */
     document.getElementById("buttonAdd").addEventListener("click", function () {
         songArr.push(new Song(
             document.getElementById("title").value,
@@ -40,12 +17,14 @@ document.addEventListener("DOMContentLoaded", function () {
         document.location.href = "index.html#ListAll";
     });
     
+    /* Button: Show Song Added */
     document.getElementById("buttonShow").addEventListener("click", function(){
         let display = songArr[0].toString();
         document.getElementById("show").value = display;
         
     });
 
+    /* Button: Clear Form Input */
     document.getElementById("buttonClear").addEventListener("click", function () {
         document.getElementById("title").value = "";
         document.getElementById("artist").value = "";
@@ -55,43 +34,44 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("rating").value = "";
     });
 
-    //sort by raiting
+    /* Button: Sort By Rating */
     document.getElementById("buttonSortRating").addEventListener("click", function () {
         songArr.sort(dynamicSort("Rating"));
         createList();
         document.location.href = "index.html#page3";
     });
 
+    /* Button: Sort By Title */
+    document.getElementById("buttonSortTitle").addEventListener("click", function () {
+        songArr.sort(dynamicSort("Title"));
+        createList();
+        document.location.href = "index.html#page3";
+    });
 
-
-    //Page before show code ----------------------------------------------------
+    /* Page 3 - List Songs */
     $(document).on("pagebeforeshow", "#page3", function (event){
         createList();
     });
 
+    /* Page 4 - Detail of Song */
     $(document).on("pagebeforeshow", "#page4", function (event) {
-        /*let localParm = localStorage.getItem("parm");
-        let localID = GetArrayPointer(localParm);*/
-
-        let localID = localStorage.getItem('parm'); //get unique id
+      let localParm = localStorage.getItem('parm');
+      let localID = GetArrayPointer(localParm);
 
         //songArr = JSON.parse(localStorage.getItem(songArr));
 
-        console.log(songArr[localID]);
-
-        //Maybe something like this?
-        //document.getElementById("oneTitle").innerHTML = "The title is: " + songArr[localID].Title;
-        document.getElementById("oneTitle").innerHTML = localID.Title
-        /* 
+        /* Display Detail of Selected Li */
+        document.getElementById("oneTitle").innerHTML = "Title: " + songArr[localID].Title;
         document.getElementById("oneArtist").innerHTML = "Artist: " + songArr[localID].Artist;
         document.getElementById("oneAlbum").innerHTML = "Album: " + songArr[localID].Album;
         document.getElementById("oneYear").innerHTML = "Year: " + songArr[localID].Year;
         document.getElementById("oneGenre").innerHTML = "Genre: " + songArr[localID].Genre;
-        document.getElementById("oneRating").innerHTML = "Rating: " + songArr[localID].Rating;*/
+        document.getElementById("oneRating").innerHTML = "Rating: " + songArr[localID].Rating;
         
     });
 }); 
 
+/*** Creat List Funtion *************************************************************/
 function createList() {
     var ulSongList = document.getElementById("ulSongList");
     ulSongList.innerHTML = "";
@@ -104,8 +84,7 @@ function createList() {
             li.innerHTML = "\"" + element.Title + "\" (Rating: " + element.Rating + ")";
             ulSongList.appendChild(li);
     });
-
-//---------------click function that jumps to #page4--------------------------------------------
+/*** Jump To Page 4 On Click *********************************************************/
     var liArray = document.getElementsByClassName("oneSong");
     Array.from(liArray).forEach(function (element) {
         element.addEventListener("click", function () {
@@ -118,7 +97,7 @@ function createList() {
     });
 };
 
-//In the createList function, gets the ID from localStorage and checks where in the array matches the stored ID
+/*** Gets ID From localStorage and checks where in the array matches the stored IDe ***/
 function GetArrayPointer(localID) {
     for (let i = 0; i < songArr.length; i++) {
         if (localID === songArr[i].ID) {
@@ -127,8 +106,7 @@ function GetArrayPointer(localID) {
     }
 }
 
-
-// Sorting Function
+/*** Sorting Function ******************************************************************/
 function dynamicSort(property) {
     return function (a, b) {
             return a[property].localeCompare(b[property]);
